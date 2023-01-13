ed25830ef62a37699d107f4a700624a36ca0b7ab
shang <shanghaisuzhou2@gmail.com>
2023-01-13 09:18:19 -0500

    parse tissue type class

--- main/parseTissueType.py
+++ main/parseTissueType.py
@@ -1,3 +1,14 @@
-import pandas
+import pandas as pd
+import os
 
-data = pandas.read_csv('C:/~/Documents/CompBi')
+class tissueBase:
+    def __init__(self, tissueType):
+        self.tissueTypeFile = tissueType
+        self.data = pd.read_csv(os.path.join('C:/~/Documents/Comp Bio 2022-23/CompBioDirectedStudy/TissueFiles/', self.tissueTypeFile), sep='\t', header=0,)
+    """inputColumns accepts an array of columns found in TissueTypes."""
+    def returnColumnData(self, inputColumns):
+        return self.data.get(columns=inputColumns, default="NA")
+    def returnAllData(self):
+        return self.data
+    def popColumns(self, inputColumns):
+        self.data.droplevel(inputColumns)
