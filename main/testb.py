6e535e6c92f2480dbe910de9c0ac5f5151896191
Watterpugg <braydenwhitlow@gmail.com>
2023-01-20 08:45:25 -0500

    No message

--- main/testb.py
+++ main/testb.py
@@ -1,33 +1,31 @@
-#importing the necessary packages
-import csv
+import sys
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
 
-#initializing an empty list to store the similarities
-similarities = []
 
-#opening the first tsv file
-with open('C:\Comp Bio\CompBioDirectedStudy\DatabaseExtract_v_1.01.csv', newline='') as tsvfile1:
-    data1 = csv.reader(tsvfile1)
-    #skipping the header row
-    next(data1)
-    #iterating through each row in the tsv file
-    for row in data1:
-        #storing the value of the chosen columns in variables
-        value1 = row[1]
-        
 
-#opening the second tsv file
-with open('C:\Comp Bio/brain.tsv', newline='') as tsvfile2:
-    data2 = csv.reader(tsvfile2, delimiter='\t')
-    #skipping the header row
-    next(data2)
-    #iterating through each row in the tsv file
-    for row in data2:
-        #storing the value of the chosen columns in variables
-        value3 = row[2]
-        #checking if the values of the chosen columns are equal
-        if value1 == value3:
-            #appending the similarity to the list
-            similarities.append(value1)
+class Window(QWidget):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.resize(400,400)
+        self.setWindowTitle("Py-CRIT")
+        self.button = button = QPushButton(icon=QIcon('C:\Comp Bio\pyTitle.png'))
+        self.button.setIconSize(QSize(550,550))
+        self.button.clicked.connect(self.onClick)
+        layout = QVBoxLayout()
+        layout.addWidget(self.button)
+        self.setLayout(layout)
 
-#printing the list of similarities
-print(similarities)
+
+    def onClick(self, parent=None):
+        Tissue = QComboBox()
+        Tissue.addItems(['brain','heart','lung'])
+        Tissue.show()
+
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
