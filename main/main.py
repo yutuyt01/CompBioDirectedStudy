1f4105c08ff5af24b88560875b0d0c31c25f7705
Tim Wilson <shanghaisuzhou2@gmail.com>
2023-02-03 06:43:53 -0500

    sirnaruledesign

--- main/main.py
+++ main/main.py
@@ -1,10 +1,13 @@
 import os
 from compare import compareClass
 from getGene import getGeneClass
+from siRNARuleDesign import siRNADesign
 
 compareTypes = compareClass("ovary.tsv", True)
 grabTool = getGeneClass()
 
+
 matchingIDs = compareTypes.compare()
 print(grabTool.getSeqEnsembl(matchingIDs))
+
 
--- main/test.py
+++ main/test.py
@@ -1,15 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
+import os
 from Bio import Entrez, SeqIO, pairwise2
 from Bio.Seq import Seq
+from siRNARuleDesign import siRNADesign
 
 
 from Bio.pairwise2 import format_alignment
 Entrez.email = "compbioproject23@gmail.com"
-
+siRNAChecker = siRNADesign(True,True,False, 50) #generates checker off ui-tei and reynolds rules
 handle = Entrez.efetch(db="nuccore",
                        id="NM_000014.6",
                        rettype="fasta",
@@ -17,6 +15,9 @@ handle = Entrez.efetch(db="nuccore",
 
 record = SeqIO.read(handle, "fasta")
 
+class search:
+    def __init__(self, sequences, siRNACheckerObject, ):
+        pass
 siRNA = Seq("")
 seedRegion = Seq("")
 geneSequence = record.seq
@@ -29,8 +30,8 @@ idList = []
 
 testRegion = Seq("")
 # real file remmed out
-#for index, record in enumerate(SeqIO.parse("../../Documents/GRCh38_latest_rna.fna", "fasta")):
-for index, record in enumerate(SeqIO.parse("../../Downloads\TestGenes.fasta", "fasta")):
+for index, record in enumerate(SeqIO.parse(os.path.join(os.getcwd(), "GRCh38_latest_rna.fna"), "fasta")):
+#for index, record in enumerate(SeqIO.parse("../../Downloads\TestGenes.fasta", "fasta")):
     assert isinstance(record.seq, object)
     sequenceArray.append(record.seq)
     idList.append(record.id)
@@ -41,34 +42,35 @@ matchArray = [0] * Seq.__len__(geneSequence)
 siRNAArray = [Seq("")] * Seq.__len__(geneSequence)
 
 while i < Seq.__len__(geneSequence):
-    #siRNA = geneSequence[i - 23:i]
-    siRNA = "CCAGGACTGCGGGAAGGCGCGGG"
-    # print(siRNA)
-    seedRegion = siRNA[1:8]
+    siRNA = geneSequence[i - 21:i]
+    
+    #siRNA = "CCAGGACTGCGGGAAGGCGCGGG"
+    print(siRNA)
+    #seedRegion = siRNA[1:8]
     # print(seedRegion)
     h = 0
     g = 7
     matches = 0
-    siRNAArray[i-23] = siRNA
+    siRNAArray[i-21] = siRNA
     while h < len(sequenceArray):
         g = 7
         while g < Seq.__len__(sequenceArray[h]):
-            pullSequence = sequenceArray[h]
-            testRegion = pullSequence[g - 7:g]
-            # print(testRegion)
-            if seedRegion == testRegion:
-                print('Match!')
-                print('Occurred at nucleotide', (g-7))
-                print('siRNA strand of ' + siRNA + ' and a base region of ' + seedRegion)
-                print('                 -------')
-                print('Occured in gene ' + idList[h])
-                print()
-                matches += 1
-                # TODO complement
+        #    pullSequence = sequenceArray[h]
+        #    testRegion = pullSequence[g - 7:g]
+        #    # print(testRegion)
+        #    if seedRegion == testRegion:
+        #        print('Match!')
+        #        print('Occurred at nucleotide', (g-7))
+        #        print('siRNA strand of ' + siRNA)
+        #        print('                 -------')
+        #        print('Occured in gene ' + idList[h])
+        #        print()
+        #         matches += 1
+        #        # TODO complement
             g += 1
         h += 1
     i += 1
-    matchArray[i-23] = matches
+    matchArray[i-21] = matches
 
 
 for i in range(5):
